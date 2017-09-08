4d97c408542c788163d2d7249d3e7a8f3eaa6008
drzz21 <drz.y2k@gmail.com>
2017-09-08 18:48:27 -0500

    Estable

--- PasoDeParametros/app/src/main/java/com/dios/pasodeparametros/Main2Activity.java
+++ PasoDeParametros/app/src/main/java/com/dios/pasodeparametros/Main2Activity.java
@@ -1,6 +1,7 @@
 package com.dios.pasodeparametros;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,6 +17,7 @@ public class Main2Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
--- PasoDeParametros/app/src/main/java/com/dios/pasodeparametros/MainActivity.java
+++ PasoDeParametros/app/src/main/java/com/dios/pasodeparametros/MainActivity.java
@@ -1,6 +1,7 @@
 package com.dios.pasodeparametros;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -42,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         agregarContactos=(Button)findViewById(R.id.btnAgregarContacto);
